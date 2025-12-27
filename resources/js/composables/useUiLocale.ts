09f50455c8c463f118246bafb208bcf11d0b291f
Mohammad Zubair Akrami <paghmane@yahoo.com>
2025-12-28 00:22:49 +0430

    enu on right or on left

--- resources/js/composables/useUiLocale.ts
+++ resources/js/composables/useUiLocale.ts
@@ -9,6 +9,7 @@ export function setUiLocale(locale: UiLocale) {
   uiLocale.value = locale;
   // Cast to any to avoid TS error, or fix by adding vue-i18n.d.ts
   (i18n.global.locale as any).value = locale;
+
 }
 
 watch(uiLocale, (lang) => {
--- resources/js/layouts/app/AppSidebarLayout.vue
+++ resources/js/layouts/app/AppSidebarLayout.vue
@@ -4,20 +4,28 @@ import AppShell from '@/components/AppShell.vue';
 import AppSidebar from '@/components/AppSidebar.vue';
 import AppSidebarHeader from '@/components/AppSidebarHeader.vue';
 import type { BreadcrumbItemType } from '@/types';
+import { computed } from 'vue'
+import { uiLocale } from '@/composables/useUiLocale'
 
+const isRtl = computed(() => uiLocale.value === 'prs' || uiLocale.value === 'ps')
+
+// dynamically set side based on locale direction
+const sidebarSide = computed(() => (isRtl.value ? 'right' : 'left'))
 interface Props {
     breadcrumbs?: BreadcrumbItemType[];
 }
+debugger;
 
 withDefaults(defineProps<Props>(), {
     breadcrumbs: () => [],
 });
+//    variant?: 'floating' | 'sidebar' | 'inset';
 </script>
 
 <template>
     <AppShell variant="sidebar">
-        <AppSidebar />
-        <AppContent variant="floating" class="overflow-x-hidden">
+        <AppSidebar  :side="sidebarSide" />
+        <AppContent variant="sidebar" class="overflow-x-hidden">
             <AppSidebarHeader :breadcrumbs="breadcrumbs" />
             <slot />
         </AppContent>
